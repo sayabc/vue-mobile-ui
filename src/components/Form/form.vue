48223bd87f233dcb6534b944a98f6f28758e9402
eyea <deyesea@gmail.com>
2018-11-11 22:40:31 +0800

    update

---
+++ src/components/Form/form.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+
+    <div class="intro">
+      <h2>Form</h2>
+      <p>表单内容 input select radio .etc</p>
+    </div>
+
+    <div class="compons_detail_list">
+      <ul>
+        <li @click="handleDialog">标题+内容</li>
+      </ul>
+    </div>
+
+    <div class="full_mask" v-if="showFullMask">
+      <div class="mask_body_container dialog_container">
+        <h4 class="mask_container_title dialog_title">Dialog Title!</h4>
+        <p class="mask_container_body dialog_boy">
+          提示信息可以单行、多行，控制住
+        </p>
+        <div class="mask_comtainers_btns">
+          <span class="mask_btn mask_btn_flex" @click="handleConfirm">确认</span>
+          <span class="mask_btn_blank"></span>
+          <span class="mask_btn mask_btn_flex" @click="handleDialog">取消</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="notes">
+      Form 元素 应用场景: 用户输入信息类型
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      showFullMask: false
+    }
+  },
+  mounted () {
+    let y = false
+    let x = y || 1
+    this.hh(x)
+  },
+  methods: {
+    hh (x) {
+      console.log(x)
+    },
+    handleDialog () {
+      this.showFullMask = !this.showFullMask
+    },
+    handleConfirm () {
+      this.showFullMask = false
+      // 处理逻辑
+    }
+  }
+}
+</script>
+
+<style scoped>
+.intro {
+  text-align: left;
+  margin-bottom: 40px;
+}
+
+.compons_detail_list {
+  padding: 10px;
+}
+ul>li {
+  width: 300px;
+  height: 50px;
+  line-height: 50px;
+  border: 1px solid gray; /*no*/
+  border-radius: 6px;
+  margin: 0 auto;
+}
+.full_mask {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(255,255,255,.5);
+}
+.mask_body_container {
+  border: 1px solid gray; /*no*/
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  padding: 8px 16px;
+  border-radius: 6px;
+}
+.mask_container_title {
+  text-align: center;
+  font-size: 14px;
+}
+.mask_container_body {
+  text-align: left;
+}
+.dialog_container {
+  padding: 20px 40px;
+}
+.dialog_boy {
+  margin: 20px;
+}
+.mask_comtainers_btns {
+  display: flex;
+  margin-bottom: 4px;
+}
+.mask_btn {
+  flex: 1;
+  padding: 6px 10px;
+}
+.mask_btn_flex {
+  border: 1px solid gray; /*no*/
+  border-radius: 8px;
+}
+.mask_btn_blank {
+  flex: 1;
+}
+.notes {
+  margin-top: 60px;
+  padding: 20px;
+  text-align: left;
+  border: 1px dotted gray; /*no*/
+  border-radius: 10px;
+}
+</style>
